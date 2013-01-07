7221d6c1a28b02ba321be85d7d3578c9aee3c6bf
Alexander Shapkin <as.zgroupsoft@gmail.com>
2013-01-07 16:22:07 +0400

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Appointments::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( dhtmlxcommon.js, dhtmlxgrid.js, dhtmlxgridcell.js, dhtmlxdataprocessor.js )
+  config.assets.precompile += %w( dhtmlxcommon.js dhtmlxgrid.js dhtmlxgridcell.js dhtmlxdataprocessor.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
